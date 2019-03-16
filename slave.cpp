7776cdfaa9c668efcf4d7bb810746829d89ff2c8
Ruger <550279039@qq.com>
2019-03-16 11:37:19 +0800

    Update slave.cpp

--- slave.cpp
+++ slave.cpp
@@ -20,41 +20,41 @@ int main() {
         if (pid > 0) continue;
         int stat = 0;
         while (recv(newMaster, &stat, sizeof(stat), 0) > 0) { 
-			int open_file = stat + 200;
-			sendSocket = socket_create();
-			send(newMaster, &open_file, sizeof(int), 0);
-			int yes = 0;
-			while (recv(newMaster, &yes, sizeof(int), 0) > 0) {
-				if (yes == 1){
-					break;
-				}
+		int open_file = stat + 200;
+		sendSocket = socket_create();
+		send(newMaster, &open_file, sizeof(int), 0);
+		int yes = 0;
+		while (recv(newMaster, &yes, sizeof(int), 0) > 0) {
+			if (yes == 1){
+				break;
 			}
-			socket_connect(sendSocket, 2022, client_ip);
-            FILE* command = NULL;
-			switch (open_file){
-				case 201: {
-					command = popen("bash shell/MEM.sh", "r");
-				}break;
-				case 202: {
-					command = popen("bash shell/DISK.sh", "r");
-				}break;
-				case 203: {
-					command = popen("bash shell/CPU.sh", "r");
-				}break;
-				case 204: {
-					command = popen("bash shell/OS_INFO.sh", "r");
-				}break;
-				case 205: {
-					command = popen("bash shell/USER.sh", "r");
-				}break;
-			}
-			char str[1024] = {0};
-			char ans[1024] = {0};
-			while(fgets(str, sizeof(str), command)) {
-					strcat(ans,str);
-			}	
-			printf("%s\n", ans);
-			send(sendSocket, ans, strlen(ans), 0);
+		}
+		socket_connect(sendSocket, 2022, client_ip);
+            	FILE* command = NULL;
+		switch (open_file){
+			case 201: {
+				command = popen("bash shell/MEM.sh", "r");
+			}break;
+			case 202: {
+				command = popen("bash shell/DISK.sh", "r");
+			}break;
+			case 203: {
+				command = popen("bash shell/CPU.sh", "r");
+			}break;
+			case 204: {
+				command = popen("bash shell/OS_INFO.sh", "r");
+			}break;
+			case 205: {
+				command = popen("bash shell/USER.sh", "r");
+			}break;
+		}
+		char str[1024] = {0};
+		char ans[1024] = {0};
+		while(fgets(str, sizeof(str), command)) {
+			strcat(ans,str);
+		}	
+		printf("%s\n", ans);
+		send(sendSocket, ans, strlen(ans), 0);
         }
     }
     return 0;
